4ec1bbf2211a9567ca08d08074f5bb3797915a67
Rony7736 <ronychakrabarty99@gmail.com>
2024-09-12 13:43:55 +0600

    completed assignment problem-4

---
+++ problem-4.js
@@ -0,0 +1,36 @@
+
+
+function calculateFinalScore(obj){
+
+    if(typeof obj !== 'object' || obj.testScore > 50 || obj.schoolGeade > 30){
+        return "Invalid Input";
+    }
+
+    let totalMark = obj.testScore + obj.schoolGeade;
+
+    if(obj.isFfamily === true){
+        totalMark = totalMark + 20;
+    }
+
+    if(obj.isFfamily === true && totalMark >= 80){
+        return true;
+    }
+    else if(obj.isFfamily === true && totalMark <= 80){
+        return false;
+    }
+    else if(obj.isFfamily === false && totalMark >= 80)
+        return true;
+    else if (!obj.isFfamily === true && totalMark < 80){
+        return false;
+    }
+}
+
+const person = {
+    name : 'Rajib',
+    testScore : 15,
+    schoolGeade : 25,
+    isFfamily : true,
+};
+
+const candidate = calculateFinalScore(person);
+console.log(candidate);
